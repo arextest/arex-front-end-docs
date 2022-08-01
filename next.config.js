4c4416b0f7420a1aa5504a25ec706b673b99c33c
张涛 <tzhangm@trip.com>
2022-08-01 19:13:49 +0800

    upd

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.js'
+  themeConfig: './theme.config.js',
+  assetPrefix: "/arex-front-end-docs", //加前缀
+  basePath: "/arex-front-end-docs", //node
 })
 module.exports = withNextra()
